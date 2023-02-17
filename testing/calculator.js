1cd17e9df0c487164689dbf04c4bcfa22915e0c0
gyozpada <galanglost@gmail.com>
2023-02-17 17:28:47 +0700

    add some comment

--- testing/calculator.js
+++ testing/calculator.js
@@ -2,7 +2,9 @@ const { suite, test, before } = intern.getPlugin('interface.tdd');
 const { assert } = intern.getPlugin('chai');
 
 suite('Kalkulator', ()=>{
+    //this function is run once before test
     before(async({ remote })=>{
+        //to open page that want to be tested
         await remote.get('https://projects.masputih.com/calculator/')
     })
 })
